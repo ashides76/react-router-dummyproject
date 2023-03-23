a0985f0d5d7c9b0d6719022ef345a86e027ff7a7
Ashish Desai <desai.ashish76@gmail.com>
2023-03-23 11:28:43 -0400

    route setup lessions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
+
+const style = {
+  margin: '1rem', 
+  padding: '0.5rem', 
+  border: '2px solid black',
+}
+
+function Home(props) {
+  return (
+    <h2 style={{...style, borderColor: 'red' }}>Home Screen</h2>
+  )
+}
+
+function Dog(props) {
+  return (
+    <h2 style={{...style, borderColor: 'blue' }}>Dog Screen</h2>
+  )
+}
+
+function About(props) {
+  return (
+    <h2 style={{...style, borderColor: 'green' }}>About Screen</h2>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <h1>My App</h1>
+      <nav>
+        <Link to='/'>Home</Link>&nbsp;
+        <Link to='dog'>Dog</Link>&nbsp;
+        <Link to='about'>About</Link>&nbsp;
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />}/> 
+        <Route path='/dog' element={<Dog />}/> 
+        <Route path='/about' element={<About />}/> 
+      </Routes>
+    </BrowserRouter>
   );
 }
 
